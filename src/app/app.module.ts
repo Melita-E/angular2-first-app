f3b54b785a7252edd1f6c3cfebb659192544dd77
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-08-20 19:33:07 +0300

    added menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import {AppComponent} from './app.component';
 import  {ColorDirective} from './color/color.directive';
 import {ModalComponent} from './modal/modal.component';
 import {ListComponent} from "./list/list.component";
+import {MainMenuComponent} from "./main-menu/main-manu.component";
 
 @NgModule({
   imports: [BrowserModule, HttpModule],
-  declarations: [AppComponent, ColorDirective, ModalComponent, ListComponent],
+  declarations: [AppComponent, ColorDirective, ModalComponent, ListComponent, MainMenuComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
