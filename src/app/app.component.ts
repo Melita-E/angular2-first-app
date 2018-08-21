f30f95806f10cbacd77e62aae6eb98f850098fdf
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-08-21 16:19:14 +0300

    added encapsulation for app component, refactored styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import  {AppService} from './app.service';
+import {ViewEncapsulation} from '@angular/core';
 import {ListComponent} from "./list/list.component";
 
 @Component({
   selector:'app',
   providers: [AppService],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
